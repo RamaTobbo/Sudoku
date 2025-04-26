5379f9a8d87cebe719c694d6de333bd72bd31384
unknown <ramatobbo5@gmail.com>
2025-04-26 11:44:52 +0300

    populate each Grid row with 9 Block components

--- src/components/grid/ndex.tsx
+++ src/components/grid/ndex.tsx
@@ -11,7 +11,11 @@ const Grid: FC = () => {
       {Children.toArray(
         [...Array(9)].map((_, rowIndex) => (
           <Row data-cy="grid-row-container">
-        
+            {Children.toArray(
+              [...Array(9)].map((_, colIndex) => (
+                <Block colIndex={colIndex} rowIndex={rowIndex} />
+              ))
+            )}
           </Row>
         ))
       )}

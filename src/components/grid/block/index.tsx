328e8fe2ea06c2228bc64eb2c130dc866657a4fb
unknown <ramatobbo5@gmail.com>
2025-05-03 13:38:01 +0300

    mapping data

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -2,13 +2,34 @@ import React, { FC } from 'react'
 
 import { Container } from './styles'
 
+
+
+import { useSelector } from 'react-redux'
+
+import { IReducer } from '../../../reducers'
+import { N } from '../../../typings'
+
+
+
 interface IProps {
   colIndex: number
   rowIndex: number
 }
 
+interface IState {
+  value: N
+}
+
 const Block: FC<IProps> = ({ colIndex, rowIndex }) => {
-  return <Container data-cy={`block-${rowIndex}-${colIndex}`} />
+  const state = useSelector<IReducer, IState>(({ grid }) => ({
+    value: grid ? grid[rowIndex][colIndex] : 0,
+  }))
+
+  return (
+    <Container data-cy={`block-${rowIndex}-${colIndex}`}>
+      {state.value === 0 ? '' : state.value}
+    </Container>
+  )
 }
 
 export default Block
